f218292141d3decc7e78c38f9f5c59c0107d3fe9
PratikHan <patrick.hanchate@gmail.com>
2019-10-13 18:11:41 -0400

    top viewpager implemented

--- MovieStream/app/src/main/java/com/bing/moviestream/MainActivity.java
+++ MovieStream/app/src/main/java/com/bing/moviestream/MainActivity.java
@@ -1,20 +1,36 @@
 package com.bing.moviestream;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    List<Slide> slides;
+    ViewPager slidePager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Slide> slides = new ArrayList<>();
+        slides = new ArrayList<>();
+        slidePager = findViewById(R.id.slide_pager);
+        slides.add(new Slide(R.drawable.a,"Spectre"));
+        slides.add(new Slide(R.drawable.b,"Avengers"));
+
+        slides.add(new Slide(R.drawable.g,"The Freedom"));
+        slides.add(new Slide(R.drawable.e,"The WolfMan"));
+
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this,slides);
+        slidePager.setAdapter(adapter);
 
     }
 }
--- MovieStream/app/src/main/java/com/bing/moviestream/ViewPagerAdapter.java
+++ MovieStream/app/src/main/java/com/bing/moviestream/ViewPagerAdapter.java
@@ -1,7 +1,11 @@
 package com.bing.moviestream;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
@@ -26,6 +30,29 @@ public class ViewPagerAdapter extends PagerAdapter {
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
         return view == object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater layoutInflater = (LayoutInflater) mcontext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View slideLayout =  layoutInflater.inflate(R.layout.slide_item,null);
+
+
+        ImageView slideImg = slideLayout.findViewById(R.id.slide_imageView);
+        TextView slideImageText = slideLayout.findViewById(R.id.slide_title);
+        slideImg.setImageResource(slides.get(position).getImage());
+        slideImageText.setText(slides.get(position).getTitle());
+
+        container.addView(slideLayout);
+
+
+        return slideLayout;
     }
 
 
