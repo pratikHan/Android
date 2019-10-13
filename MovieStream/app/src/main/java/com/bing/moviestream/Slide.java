c0a3fe5f29c7dd26825e1dbd17bd9c4c4846c2fd
PratikHan <patrick.hanchate@gmail.com>
2019-10-12 21:18:40 -0400

    MS - created slide

--- MovieStream/app/src/main/java/com/bing/moviestream/Slide.java
+++ MovieStream/app/src/main/java/com/bing/moviestream/Slide.java
@@ -1,4 +1,30 @@
 package com.bing.moviestream;
 
 public class Slide {
+
+
+    int image;
+    String title;
+
+
+    public Slide(int image, String title) {
+        this.image = image;
+        this.title = title;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
--- MovieStream/app/src/main/java/com/bing/moviestream/ViewPagerAdapter.java
+++ MovieStream/app/src/main/java/com/bing/moviestream/ViewPagerAdapter.java
@@ -1,4 +1,32 @@
 package com.bing.moviestream;
 
-public class ViewPagerAdapter {
+import android.content.Context;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    Context mcontext;
+    List<Slide> slides;
+
+    public ViewPagerAdapter(Context context, List<Slide> slides) {
+        this.mcontext = context;
+        this.slides = slides;
+    }
+
+    @Override
+    public int getCount() {
+        return slides.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+
 }
