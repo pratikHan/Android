b3c14758949fe1ee3063a32a35ec644275aaa8a3
PratikHan <patrick.hanchate@gmail.com>
2019-10-29 21:08:59 -0400

    Implemented Tabs

--- MovieStream/app/src/main/java/com/bing/moviestream/MainActivity.java
+++ MovieStream/app/src/main/java/com/bing/moviestream/MainActivity.java
@@ -6,19 +6,27 @@ import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     List<Slide> slides;
     ViewPager slidePager;
+    private TabLayout tab_indicator;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tab_indicator = findViewById(R.id.indicator_tabs);
 
         slides = new ArrayList<>();
         slidePager = findViewById(R.id.slide_pager);
@@ -32,5 +40,35 @@ public class MainActivity extends AppCompatActivity {
         ViewPagerAdapter adapter = new ViewPagerAdapter(this,slides);
         slidePager.setAdapter(adapter);
 
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MainActivity.SlideTimer(),7000,3000);
+
+        tab_indicator.setupWithViewPager(slidePager,true);
+
+
+
+    }
+
+
+    class SlideTimer extends TimerTask{
+
+
+        @Override
+        public void run() {
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (slidePager.getCurrentItem() < slides.size() -1){
+
+                        slidePager.setCurrentItem(slidePager.getCurrentItem()+1);
+                    }
+                    else {
+                        slidePager.setCurrentItem(0);
+                    }
+                }
+            });
+        }
     }
 }
